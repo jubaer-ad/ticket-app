e94f1c25a388be3907ec23aaac868b12de3db064
jubaerad <jubaerad1@gmail.com>
2024-08-26 21:35:18 +0600

    git 1st user commit

---
+++ app/(components)/DeleteBlock.jsx
@@ -0,0 +1,10 @@
+import { faX } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const DeleteBlock = () => {
+  return (
+    <FontAwesomeIcon icon={faX} className="text-red-500 hover:text-red-300"/>
+  )
+}
+
+export default DeleteBlock

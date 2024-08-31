b6088ad9b0ff303320d30c1825c592db9d554370
A N M JUBAER <anm.jubaer@technonext.com>
2024-08-31 23:58:35 +0600

    ticket delete complete

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,9 +1,22 @@
+"use client"
 import { faX } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-const DeleteBlock = () => {
+import {useRouter} from "next/navigation"
+
+const DeleteBlock = ({id}) => {
+  const router = useRouter()
+  const handleOnClick = async () => {
+    const rsp = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      method: "DELETE"
+    })
+    if (rsp.ok) {
+      router.refresh()
+    }
+  }
+  
   return (
-    <FontAwesomeIcon icon={faX} className="text-red-500 hover:text-red-300"/>
+    <FontAwesomeIcon icon={faX} className="text-red-500 hover:text-red-300" onClick={handleOnClick}/>
   )
 }
 
